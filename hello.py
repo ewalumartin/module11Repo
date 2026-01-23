3a0627294872fb68213891fd5ab81a55b410ec65
ewalumartin <ewalumartin@gmail.com>
2026-01-23 10:38:36 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 11/15/2024
 # A simple Python program of print a message
 
-print("hello, world!")
+# martin ewalu
+# 1/23/2026
+# added a new message
+
+print("Hello, world!")
+
+print("welome to Github!")
